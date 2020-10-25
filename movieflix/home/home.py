bc3fc475ebe5ec921104fd5d788a69529e8c7cd7
hillaro <69489040+hillaro@users.noreply.github.com>
2020-10-25 17:48:32 +1300

    Add files via upload

---
+++ movieflix/home/home.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, render_template
+
+import movieflix.utilities.utilities as utilities
+
+
+home_blueprint = Blueprint(
+    'home_bp', __name__)
+
+
+@home_blueprint.route('/', methods=['GET'])
+def home():
+    return render_template(
+        'home/home.html',
+        selected_articles=utilities.get_selected_articles(),
+        #wtf bro
+        tag_urls=utilities.get_tags_and_urls()
+    )
